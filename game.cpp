abc0a6f9b522c36e1e5b93996c90b88bb7898e09
magicmanjack <32428006+magicmanjack@users.noreply.github.com>
2018-12-04 21:01:56 +1300

    Added struct and functions.
    A struct was created to record the status of the input and can be referenced thoughout the program. An update and draw method was created.

--- game.cpp
+++ game.cpp
@@ -8,18 +8,29 @@ const int G_WIDTH = 5, G_HEIGHT = 5; // Grid dimensions.
 const int FPS = 2;
 
 void update();
+void draw();
+
+struct KeyInfo {
+	char c;
+	bool pressed;
+} kInf;
 
 int main() {
 	bool loop = true;
 	while(loop) {
-		if(kbhit()) {
-			char c = getch();
+		if(kbhit()) { 
+			kInf.pressed = true;
+			kInf.c = getch(); // Retrieves input.
 			// Key press checking.
-			if(c == 27) {
+			if(kInf.c == 27) { // A char with the value of 27 is ESC.
 				loop = false; // ESC exits the gameloop and closes the game.
 			}
+		} else {
+			kInf.pressed = false;
 		}
 		update();
+		system("CLS");
+		draw();
 		Sleep(1000 / FPS);
 	}
 	return 0;
@@ -28,5 +39,16 @@ int main() {
 void update() {
 	// All game attributes are updated.
 	
+}
+
+void draw() {
+	// The updated game is then pushed to the console.
+	cout << "Key ";
+	if(kInf.pressed) {
+		cout << "pressed";
+	} else {
+		cout << "released";
+	}
+	cout << ", Key num: " << static_cast<int>(kInf.c) << endl;
 }
 	
